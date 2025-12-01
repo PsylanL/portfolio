13b3b8104f13f2fca8b5551e88b1cbc0a7eb3e58
PsylanL <dylan_lopez82201@elpoli.edu.co>
2025-12-01 18:17:59 -0500

    Added dock visuals

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Navbar from "#components/Navbar"
 import Welcome from "#components/Welcome"
-
+import Dock from "#components/Dock"
 export {
     Navbar,
-    Welcome
+    Welcome,
+    Dock
 }
